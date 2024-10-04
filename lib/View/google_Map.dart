6011a94c6ddbfde4e8b9fb20e9444c1bdd63011e
Suraj Yadav <surajyadav200701@gmail.com>
2024-10-04 23:02:21 +0530

    Succefully Got the Current Location

--- lib/View/google_Map.dart
+++ lib/View/google_Map.dart
@@ -12,11 +12,16 @@ class GoogleMapScreen extends StatefulWidget {
 class _GoogleMapScreenState extends State<GoogleMapScreen> {
   final locationController = Location();
 
-
   static const googleplex = LatLng(28.679079, 77.069710);
   static const mountainVeiw = LatLng(25.758503, 84.148911);
 
-  
+  LatLng? currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async => await fetchLocation());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,37 +45,37 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
         },
       ),
     );
+  }
 
-    Future<void> fetchLocation() async {
-      bool serviceEnabled;
-      PermissionStatus permissisonGranted;
+  Future<void> fetchLocation() async {
+    bool serviceEnabled;
+    PermissionStatus permissisonGranted;
 
-      serviceEnabled = await locationController.serviceEnabled();
-      if (serviceEnabled) {
-        serviceEnabled = await locationController.requestService();
-      } else {
+    serviceEnabled = await locationController.serviceEnabled();
+    if (serviceEnabled) {
+      serviceEnabled = await locationController.requestService();
+    } else {
+      return;
+    }
+
+    permissisonGranted = await locationController.hasPermission();
+
+    if (permissisonGranted == PermissionStatus.denied) {
+      permissisonGranted = await locationController.requestPermission();
+      if (permissisonGranted != PermissionStatus.granted) {
         return;
       }
-
-
-      permissisonGranted = await locationController.hasPermission();
-
-      if (permissisonGranted == PermissionStatus.denied) {
-        permissisonGranted = await locationController.requestPermission();
-        if (permissisonGranted != PermissionStatus.granted) {
-          return;
-        }
-      }
-
-
-      locationController.onLocationChanged.listen((currentLocation) {
-        if(currentLocation != null && currentLocation.latitude != null) {
-          setState(() {
-            curr
-          });
-        }
-      });
-
     }
+
+    locationController.onLocationChanged.listen((currentLocation) {
+      if (currentLocation != null && currentLocation.latitude != null) {
+        setState(() {
+          currentPosition =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        });
+
+        print(currentLocation);
+      }
+    });
   }
 }
