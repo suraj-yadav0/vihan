5adf05b1321f3b1c942f66a6ca7819bb20c222cf
Suraj Yadav <surajyadav200701@gmail.com>
2024-10-04 22:53:18 +0530

    Writing Code for Current ocation

--- lib/View/google_Map.dart
+++ lib/View/google_Map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class GoogleMapScreen extends StatefulWidget {
   const GoogleMapScreen({super.key});
@@ -9,27 +10,67 @@ class GoogleMapScreen extends StatefulWidget {
 }
 
 class _GoogleMapScreenState extends State<GoogleMapScreen> {
+  final locationController = Location();
+
+
   static const googleplex = LatLng(28.679079, 77.069710);
   static const mountainVeiw = LatLng(25.758503, 84.148911);
 
+  
+
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: const CameraPosition(
-        target: googleplex,
-        zoom: 14,
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: googleplex,
+          zoom: 14,
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId('sourceLocation'),
+            icon: BitmapDescriptor.defaultMarker,
+            position: googleplex,
+          ),
+          const Marker(
+            markerId: MarkerId('destinationLocation'),
+            icon: BitmapDescriptor.defaultMarker,
+            position: mountainVeiw,
+          ),
+        },
       ),
-      markers: {
-      const  Marker(markerId: MarkerId('sourceLocation'),
-        icon: BitmapDescriptor.defaultMarker,
-        position: googleplex,
-        ),
-
-        const  Marker(markerId: MarkerId('destinationLocation'),
-        icon: BitmapDescriptor.defaultMarker,
-        position: mountainVeiw,
-        ),
-      },
     );
+
+    Future<void> fetchLocation() async {
+      bool serviceEnabled;
+      PermissionStatus permissisonGranted;
+
+      serviceEnabled = await locationController.serviceEnabled();
+      if (serviceEnabled) {
+        serviceEnabled = await locationController.requestService();
+      } else {
+        return;
+      }
+
+
+      permissisonGranted = await locationController.hasPermission();
+
+      if (permissisonGranted == PermissionStatus.denied) {
+        permissisonGranted = await locationController.requestPermission();
+        if (permissisonGranted != PermissionStatus.granted) {
+          return;
+        }
+      }
+
+
+      locationController.onLocationChanged.listen((currentLocation) {
+        if(currentLocation != null && currentLocation.latitude != null) {
+          setState(() {
+            curr
+          });
+        }
+      });
+
+    }
   }
 }
