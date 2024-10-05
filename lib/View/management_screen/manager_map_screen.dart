4faae8580be86d0360f7afee29cea5f8d907bdc0
Suraj Yadav <surajyadav200701@gmail.com>
2024-10-05 10:37:07 +0530

    Completed Adding Map to Manager Position

--- lib/View/management_screen/manager_map_screen.dart
+++ lib/View/management_screen/manager_map_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:vihan/Data/const.dart';
 import 'package:vihan/View/management_screen/manager_home/manager_find_map_screen.dart';
 import 'package:vihan/utils/colors.dart';
 
@@ -10,6 +14,27 @@ class ManagerMapScreen extends StatefulWidget {
 }
 
 class _UserHomeScreenState extends State<ManagerMapScreen> {
+    final locationController = Location();
+
+  static const googleplex = LatLng(28.679079, 77.069710);
+  static const mountainVeiw = LatLng(25.758503, 84.148911);
+
+  LatLng? currentPosition;
+  Map<PolylineId, Polyline> polylines = {};
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) async => await fetchLocation());
+  }
+
+  Future<void> initializeMap() async {
+    await fetchLocation();
+    final coordinates = await fetchPolylinePoints();
+    genratepPolyLineFromPoints(coordinates);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -133,5 +158,78 @@ class _UserHomeScreenState extends State<ManagerMapScreen> {
         ],
       ),
     );
+  }
+
+  
+  Future<void> fetchLocation() async {
+    bool serviceEnabled;
+    PermissionStatus permissisonGranted;
+
+    serviceEnabled = await locationController.serviceEnabled();
+    if (serviceEnabled) {
+      serviceEnabled = await locationController.requestService();
+    } else {
+      return;
+    }
+
+    permissisonGranted = await locationController.hasPermission();
+
+    if (permissisonGranted == PermissionStatus.denied) {
+      permissisonGranted = await locationController.requestPermission();
+      if (permissisonGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    locationController.onLocationChanged.listen((currentLocation) {
+      if (currentLocation != null && currentLocation.latitude != null) {
+        setState(() {
+          currentPosition =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        });
+
+        print(currentLocation);
+      }
+    });
+  }
+
+  Future<List<LatLng>> fetchPolylinePoints() async {
+    final polyLinePoints = PolylinePoints();
+
+    final result = await polyLinePoints.getRouteBetweenCoordinates(
+      googleApiKey: googleMapsApiKey,
+      request: PolylineRequest(
+        origin: PointLatLng(googleplex.latitude, googleplex.longitude),
+        destination: PointLatLng(mountainVeiw.latitude, mountainVeiw.longitude),
+        mode: TravelMode.driving,
+       
+      ),
+    );
+
+    if (result.points.isNotEmpty) {
+      return result.points
+          .map(
+            (point) => LatLng(point.latitude, point.longitude),
+          )
+          .toList();
+    } else {
+      debugPrint(result.errorMessage);
+      return [];
+    }
+  }
+
+  Future<void> genratepPolyLineFromPoints(
+      List<LatLng> polylineCoordinates) async {
+    const id = PolylineId('polyline');
+
+    final polyline = Polyline(
+        polylineId: id,
+        color: Colors.blueAccent,
+        points: polylineCoordinates,
+        width: 5);
+
+    setState(() {
+      polylines[id] = polyline;
+    });
   }
 }
