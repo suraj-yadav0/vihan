59ce8bf4b88cfd3bba8ec3ca96901840ea3e786e
Suraj Yadav <surajyadav200701@gmail.com>
2024-11-29 21:12:54 +0530

    Logout Button Bhi Craete Kiya

--- lib/View/user_screen/user_profile_screen.dart
+++ lib/View/user_screen/user_profile_screen.dart
@@ -14,7 +14,10 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: SizedBox(),
-        title: const Text("P R O F I L E",style: TextStyle(color: Colors.white, fontSize: 24),),
+        title: const Text(
+          "P R O F I L E",
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
         centerTitle: true,
         backgroundColor: orangeColor,
       ),
@@ -150,6 +153,37 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                       child: Center(
                         child: Text(
                           "Update",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: SizedBox(
+                width: double.infinity,
+                child: Material(
+                  color: orangeColor,
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      child: Center(
+                        child: Text(
+                          "Logout",
                           style: TextStyle(
                               color: Colors.white,
                               fontSize: 22,
