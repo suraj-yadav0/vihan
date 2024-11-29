24fe26fd0446dc4eaf460b95744f012fc423f5f1
Suraj Yadav <surajyadav200701@gmail.com>
2024-11-29 20:57:34 +0530

    Corrected User Home Screen

--- lib/View/user_screen/user_home_screen.dart
+++ lib/View/user_screen/user_home_screen.dart
@@ -14,8 +14,15 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     
       appBar: AppBar(
-        title: const Text("Vihan"),
+         leading: IconButton(
+        icon: const Icon(Icons.menu, color: Colors.white),
+        onPressed: () {
+          // Add drawer here
+        },
+      ),
+        title: const Text("V I H A N", style: TextStyle(color: Colors.white, fontSize: 24)),
         centerTitle: true,
         backgroundColor: orangeColor,
       ),
@@ -38,12 +45,12 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: const Row(
                 children: [
-                  Icon(Icons.search, color: Colors.black),
+                  Icon(Icons.search, color: Colors.white),
                   SizedBox(width: 10),
                   Text(
                     'Search Location',
                     style: TextStyle(
-                      color: Colors.purple,
+                      color: Colors.orange,
                       fontSize: 16,
                     ),
                   ),
@@ -58,13 +65,15 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
             child: Column(
               children: [
                 FloatingActionButton(
+                  backgroundColor: Colors.orange,
                   onPressed: () {},
                   mini: true,
                   heroTag: 'centerButton',
-                  child: const Icon(Icons.center_focus_strong_rounded),
+                  child: const Icon(Icons.center_focus_strong_rounded,color: Colors.white,),
                 ),
                 const SizedBox(height: 10),
                 FloatingActionButton(
+                  backgroundColor: Colors.orange,
                   onPressed: () {
                     Navigator.push(
                       context,
@@ -75,7 +84,7 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
                   },
                   mini: true,
                   heroTag: 'directionButton',
-                  child: const Icon(Icons.directions),
+                  child: const Icon(Icons.directions, color: Colors.white,),
                 ),
               ],
             ),
@@ -85,10 +94,11 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
             left: 20,
             bottom: 50,
             child: FloatingActionButton(
+                backgroundColor: Colors.orange,
               onPressed: () {},
               mini: true,
               heroTag: 'trafficButton',
-              child: const Icon(Icons.traffic),
+              child: const Icon(Icons.traffic,color : Colors.white),
             ),
           ),
           // Top Right floating action button
@@ -96,12 +106,13 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
             top: 120,
             right: 20,
             child: FloatingActionButton(
+              backgroundColor: Colors.orange,
               onPressed: () {},
               mini: true,
               heroTag: 'compassButton',
               child: const Icon(
                 Icons.arrow_circle_down, // add correct icon of compass
-              ),
+             color: Colors.white, ),
             ),
           ),
         ],
